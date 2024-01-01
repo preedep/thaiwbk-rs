89f4278f38e20f60587f4d89b0a914085d6c8f98
Preedee Ponchevin <nickmsft@mac.com>
2024-01-01 11:34:09 +0700

    Add thai dictionary

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+use std::fs::read_to_string;
+use std::time::Instant;
 use log::debug;
+
+use crate::trie::thai_word_trie::TrieNode;
 
 mod trie;
 
 fn main() {
     pretty_env_logger::init();
     debug!("Starting up");
+    let filename = "lexitron.txt";
+    let mut trie = TrieNode::new();
 
+    let before = Instant::now();
+    for line in read_to_string(filename).unwrap().lines() {
+        trie.insert(&line.to_string());
+    }
+    debug!("Elapsed insert time: {:.2?}", before.elapsed());
+
+    let before = Instant::now();
+    let results = trie.words_with_prefix(&"กลม".to_string());
+    let elapsed = before.elapsed();
+
+    for result in results {
+        println!("{}", result);
+    }
+
+    debug!("Elapsed words_with_prefix time: {:.2?}", elapsed);
 }
--- src/trie/thai_word_trie.rs
+++ src/trie/thai_word_trie.rs
@@ -12,5 +12,66 @@ impl TrieNode {
             is_word: false,
         }
     }
+    pub fn insert(&mut self, word: &String) {
+        let mut current_node = self;
+        for ch in word.chars() {
+            current_node = current_node.children.entry(ch).or_insert(TrieNode::new());
+        }
+        current_node.is_word = true;
+    }
+    pub fn search(&self, word: &String) -> bool {
+        let mut current_node = self;
+        for ch in word.chars() {
+            match current_node.children.get(&ch) {
+                Some(node) => current_node = node,
+                None => return false,
+            }
+        }
+        current_node.is_word
+    }
+    pub fn words_with_prefix(&self, prefix: &String) -> Vec<String> {
+        let mut current_node = self;
+        for ch in prefix.chars() {
+            match current_node.children.get(&ch) {
+                Some(node) => current_node = node,
+                None => return vec![],
+            }
+        }
+        let mut words = vec![];
+        current_node.words_with_prefix_helper(prefix, &mut words);
+        words
+    }
+    pub fn words_with_suffix(&self, suffix: &String) -> Vec<String> {
+        let mut current_node = self;
+        for ch in suffix.chars().rev() {
+            match current_node.children.get(&ch) {
+                Some(node) => current_node = node,
+                None => return vec![],
+            }
+        }
+        let mut words = vec![];
+        current_node.words_with_suffix_helper(suffix, &mut words);
+        words
+    }
+    fn words_with_suffix_helper(&self, suffix: &String, words: &mut Vec<String>) {
+        if self.is_word {
+            words.push(suffix.chars().rev().collect());
+        }
+        for (ch, node) in &self.children {
+            let mut suffix = suffix.clone();
+            suffix.push(*ch);
+            node.words_with_suffix_helper(&suffix, words);
+        }
+    }
+    fn words_with_prefix_helper(&self, prefix: &String, words: &mut Vec<String>) {
+        if self.is_word {
+            words.push(prefix.clone());
+        }
+        for (ch, node) in &self.children {
+            let mut prefix = prefix.clone();
+            prefix.push(*ch);
+            node.words_with_prefix_helper(&prefix, words);
+        }
+    }
 }
 
